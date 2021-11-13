243384a16d2ebc6a03f714a4bf9a620b74a30f17
TheFakeWater <thefakewater@protonmail.com>
2021-11-13 14:55:23 +0100

    fix: csrf token

--- src/structures/Conversation.ts
+++ src/structures/Conversation.ts
@@ -1,6 +1,4 @@
-
-import {utils} from '../util/Util';
-import {User} from './User';
+import { User } from "./User";
 
 /**
  * Class representing a conversation
@@ -9,22 +7,15 @@ export class Conversation {
   id: number;
   members: User[];
 
-
   /**
    * Send a message to a conversation
    * @param {string} message The message to send
    */
   async send(message: string) {
-    await utils.getCSRFToken();
-    const config = {
-      headers: {
-        'x-csrf-token': utils.token,
-      },
-    };
     const payload = {
       message: message,
       conversationId: this.id,
     };
-    await global.axios.post('https://chat.roblox.com/v2/send-message', payload, config);
+    await global.axios.post("https://chat.roblox.com/v2/send-message", payload);
   }
 }
