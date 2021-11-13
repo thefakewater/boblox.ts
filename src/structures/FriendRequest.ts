402db0d31be18ad7efd4419dd32481025b898481
TheFakeWater <thefakewater@protonmail.com>
2021-11-13 14:58:22 +0100

    fix: csrf token retrieval

--- src/structures/FriendRequest.ts
+++ src/structures/FriendRequest.ts
@@ -23,12 +23,9 @@ export class FriendRequest {
    * Decline a friend request
    */
   async decline() {
-    await utils.getCSRFToken();
-    const config = {
-      headers: {
-        'x-csrf-token': utils.token,
-      },
-    };
-    await global.axios.post(`https://friends.roblox.com/v1/users/${this.author.id}/decline-friend-request`, {}, config);
+    await global.axios.post(
+      `https://friends.roblox.com/v1/users/${this.author.id}/decline-friend-request`,
+      {}
+    );
   }
 }
