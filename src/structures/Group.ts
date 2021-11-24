d79430536f251b5e38d7365e3a9e827b226d9dbb
TheFakeWater <thefakewater@protonmail.com>
2021-11-24 16:03:40 +0100

    docs: group alpha

--- src/structures/Group.ts
+++ src/structures/Group.ts
@@ -2,17 +2,32 @@ import { Base } from "./Base";
 import { GroupShout } from "./GroupShout";
 import { User } from "./User";
 
+/**
+ * Represents a Roblox group
+ * @alpha
+ */
 export class Group extends Base {
+  /**the group's id*/
   readonly id: number;
+  /**the group's name*/
   readonly name: string;
+  /**the group's description*/
   readonly description: string;
-  readonly owner;
+  /**the group's description*/
+  readonly owner: User;
   memberCount: number;
   readonly isBuilderClubOnly: boolean;
   readonly publicEntryAllowed: boolean;
   readonly isLocked: boolean;
+  /**the group's description*/
   shout: GroupShout;
 
+  /**
+   * Creates a new group with the json response from API call.
+   * @param client - the client that instancied this class
+   * @param data - the json response from API call
+   * @internal
+   */
   constructor(client, data) {
     super(client);
     this.id = data.id;
--- src/structures/GroupShout.ts
+++ src/structures/GroupShout.ts
@@ -1,11 +1,22 @@
 import { Base } from "./Base";
 
+/**
+ * Represents a group Shout.
+ * The group shout is a message that shows to everyone in the group.
+ * @alpha
+ */
 export class GroupShout extends Base {
   body: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   poster: any;
   readonly created: Date;
   readonly updated: Date;
+  /**
+   * Creates a new group shout with the json response from API call.
+   * @param client - the client that instancied this class
+   * @param data - the json response from API call
+   * @internal
+   */
   constructor(client, data) {
     super(client);
     if ("body" in data) this.body = data.body;
